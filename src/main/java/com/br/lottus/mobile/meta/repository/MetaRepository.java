ff27785222e9a05a720cbe1244de33edb9fe2530
nathannolacio <nathannolacio04@gmail.com>
2026-04-24 03:20:29 -0300

    :sparkles: feat - desvincular aluno

--- src/main/java/com/br/lottus/mobile/meta/repository/MetaRepository.java
+++ src/main/java/com/br/lottus/mobile/meta/repository/MetaRepository.java
@@ -3,6 +3,8 @@ package com.br.lottus.mobile.meta.repository;
 import com.br.lottus.mobile.meta.entity.Meta;
 import com.br.lottus.mobile.meta.entity.StatusMeta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -15,4 +17,16 @@ public interface MetaRepository extends JpaRepository<Meta, Long> {
 
     List<Meta> findByAlunoIdAndStatusAndDataInicioLessThanEqualAndDataFimGreaterThanEqual(
             Long alunoId, StatusMeta status, LocalDate inicio, LocalDate fim);
+
+    @Modifying
+    @Query("UPDATE Meta m SET m.status = :statusArquivada " +
+            "WHERE m.criadoPor.id = :usuarioId " +
+            "AND m.aluno.id = :alunoId " +
+            "AND m.status != :statusConcluida")
+    void arquivarMetasPaiAluno(
+            Long usuarioId,
+            Long alunoId,
+            StatusMeta statusArquivada,
+            StatusMeta statusConcluida
+    );
 }
